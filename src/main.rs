dfa81393316d996e7b955c2226891cb804c3fb98
marcus_oa <marcus.official1@gmail.com>
2021-01-26 19:15:24 +0000

    Some simple refactoring for cleaner code

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,31 @@ fn main() {
     // create said type
     let args: Vec<String> = env::args().collect();
 
-    // Note: &args[0] = `target/debug/minigrep`
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+// Necessary abstraction
+struct Config {
+    query: String,
+    filename: String
+}
+
+// Note: As a rule we keep the argument passing from command
+// line within the main.rs file as long as it relatively short
+fn parse_config(args: &[String]) -> Config {
+    // Note: &args[0] = `target/debug/minigrep`
+    // Note+: Clone ensures that we don't violate rust rules
+    // of taking ownership of the values passed into the fn
+    let query = args[1].clone;
+    let filename = args[2].clone;
+
+    Config { query, filename }
 }
